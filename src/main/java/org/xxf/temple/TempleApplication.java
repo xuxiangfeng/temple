b0884c69922923df473ec87dd5289aa89398453f
徐向峰 <whutxxf@163.com>
2019-01-19 10:54:25 +0800

    创建模板

---
+++ src/main/java/org/xxf/temple/TempleApplication.java
@@ -0,0 +1,14 @@
+package org.xxf.temple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempleApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/org/xxf/temple/TempleApplicationTests.java
@@ -0,0 +1,17 @@
+package org.xxf.temple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TempleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
